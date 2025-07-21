7f5e6463f73f9c9c98322a77137a4e3fb0bc0c64
Thais Rodrigues Carvalho Dias <thais.r.carvalho@animaeducacao.com.br>
2025-07-21 20:14:20 -0300

    feat:ajustes

--- Dockerfile
+++ Dockerfile
@@ -14,8 +14,12 @@ RUN apt-get update && apt-get install -y \
 RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
 
+ENV DD_SERVICE=bitcoin-lstm-api
+ENV DD_ENV=prod
+ENV DD_SITE=datadoghq.com
+
 # Copie o restante do código da aplicação para o diretório de trabalho
 COPY . .
 
 # Comando para rodar a aplicação
-CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8080", "--workers","4", "--loop", "uvloop", "--http", "httptools"]
+CMD ["ddtrace-run", "uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8080", "--workers","4", "--loop", "uvloop", "--http", "httptools"]
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 import os
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi import Response
 from datetime import datetime
 
@@ -12,6 +12,8 @@ from pydantic import BaseModel
 import numpy as np
 from tensorflow.keras.models import load_model
 import joblib
+from ddtrace import patch_all; patch_all()
+
 
 app = FastAPI(
     title="Bitcoin LSTM Predictor API",
@@ -30,8 +32,9 @@ class PriceRequest(BaseModel):
 @app.post("/predict")
 def predict_price(request: PriceRequest):
     prices = request.prices
-
+    send_log_to_datadog(f"Recebida previsão com {len(prices)} valores.", level="info")
     if not isinstance(prices, list) or len(prices) < WINDOW_SIZE:
+        send_log_to_datadog("Envie ao menos {WINDOW_SIZE} preços no formato de lista.", level="error")
         raise HTTPException(
             status_code=400, 
             detail=f"Envie ao menos {WINDOW_SIZE} preços no formato de lista."
@@ -42,6 +45,7 @@ def predict_price(request: PriceRequest):
     X_input = prices_scaled[-WINDOW_SIZE:].reshape(1, WINDOW_SIZE, 1)
     pred_scaled = model.predict(X_input)
     pred = scaler.inverse_transform(pred_scaled)
+    send_log_to_datadog("Previsão realizada com sucesso!", level="info")
     return {"predicted_close": float(pred[0, 0])}
 
 @app.get("/healthcheck")
@@ -51,6 +55,34 @@ def healthcheck():
         "message": "OK",
         "timestamp": datetime.utcnow().isoformat()
     }
+
+@app.exception_handler(Exception)
+async def exception_handler(request: Request, exc: Exception):
+    send_log_to_datadog(f"Erro na API: {str(exc)}", level="error")
+    return {"detail": "Erro interno na API"}
+
+import requests
+import json
+
+# Exemplo de uso:
+DATADOG_API_KEY = "be3f70070c8f88b11511ac9750d2af8b"
+def send_log_to_datadog(message, level="info"):
+    url = "https://http-intake.logs.datadoghq.com/v1/input"
+    headers = {
+        "Content-Type": "application/json",
+        "DD-API-KEY": DATADOG_API_KEY
+    }
+    log_entry = {
+        "message": message,
+        "ddsource": "python",
+        "service": "bitcoin-lstm-api",
+        "status": level
+    }
+    try:
+        requests.post(url, headers=headers, data=json.dumps(log_entry), timeout=3)
+    except Exception as e:
+        print(f"Falha ao enviar log para Datadog: {e}")
+
 
 
 @app.get("/")
